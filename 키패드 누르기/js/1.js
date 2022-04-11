5d3aed9ae3ad3d6e3b237485c38a4212ce9349b5
stack0801 <stack0801@gmail.com>
2022-04-11 19:57:36 +0900

    키패드 누르기

---
+++ 키패드 누르기/js/1.js
@@ -0,0 +1,58 @@
+numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+hand = "left"	
+
+console.log(solution(numbers, hand))
+
+function solution(numbers, hand) {
+    let answer = '';
+    
+    let left = 10
+    let right = 12
+    
+    for(let num of numbers) {
+        
+        if(num == 0)
+            num = 11
+        
+        let column = num % 3
+        
+        if(column == 1) {
+            answer += 'L'
+            left = num
+        }
+        else if(column == 0) {
+            answer += 'R'
+            right = num
+        }
+        else if(column == 2) {
+            const distance_L = distance(left, num)
+            const distance_R = distance(right, num)
+            
+            if(distance_L < distance_R) {
+                answer += 'L'
+                left = num
+            }
+            else if(distance_L > distance_R) {
+                answer += 'R'
+                right = num
+            }
+            else if (distance_L == distance_R) {
+                if(hand == "left") {
+                    answer += 'L'
+                    left = num
+                }
+                else if (hand == "right") {
+                    answer += 'R'
+                    right = num
+                }
+            }
+        }
+            
+    }
+    return answer;
+}
+
+function distance(n1, n2) {
+    return Math.abs((n1 - 1) % 3 - (n2 - 1) % 3) + Math.abs(parseInt((n1 - 1) / 3) - parseInt((n2 - 1) / 3))
+}
+
