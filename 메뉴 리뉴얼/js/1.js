fbc4f097430872edb58e7b729f95103db3882aa6
stack0801 <stack0801@gmail.com>
2022-04-20 03:28:03 +0900

    메뉴 리뉴얼

---
+++ 메뉴 리뉴얼/js/1.js
@@ -0,0 +1,60 @@
+function solution(orders, course) {
+    let list = {}
+    orders.map((order, i) => orders[i] = order.split('').sort().join(''))
+    orders.map((order1, i1) => {
+        orders.map((order2, i2) => {
+            if(i1 != i2) {
+                let obj = intersection(order1, order2)
+                for (let i in obj)
+                    (list[i] == undefined) ? list[i] = obj[i] : list[i] += obj[i]
+            }
+        })
+    })
+    return courses_n(list, course)
+}
+
+function courses_n(list, course) {
+    let answer = []
+    let keys = Object.keys(list)
+    let max
+
+    course.map((n) => {
+        max = 0
+        keys.map((k) => {
+            if(k.length != n)
+                return
+            if(list[k] > max)
+                max = list[k]
+        })
+        keys.map((k) => {
+            if(list[k] == max && k.length == n)
+                answer.push(k)
+        })
+    })
+
+    return answer.sort()
+}
+
+function intersection(str1, str2) {
+    let list = []
+    for(c1 of str1) {
+        for(c2 of str2) {
+            if(c1 == c2) 
+                list.push(c1)
+        }
+    }
+    result = {}
+    tree(list, list.length, '', result)
+    return result
+}
+
+function tree(list, n, arr, result) {
+    if(arr.length > 1)
+        result[arr] = 1
+    if(n == 0)
+        return
+    tree(list, n - 1, arr + list[list.length - n], result)
+    tree(list, n - 1, arr, result)
+}
+
+// +7
---
+++ 메뉴 리뉴얼/js/2.js
@@ -0,0 +1,30 @@
+function solution(orders, course) {
+    const answer = []
+    const list = []
+    orders = orders.map(order => order.split('').sort())
+    course.map(c => list[c] = {})
+    orders.map(order => {
+        let count = Math.pow(2, order.length)
+        while(--count > 2) {
+            let str = ''
+            for(let i = 0; i < order.length; i++) {
+                if(count & Math.pow(2, order.length - i - 1))
+                    str += order[i]
+            }
+            if(list[str.length])
+                !list[str.length][str] ? list[str.length][str] = 1 : list[str.length][str] += 1
+        }
+    })
+    list.map(layer => {
+        let max = 0
+        for(let i in layer) {
+            if(layer[i] > max)
+                max = layer[i]
+        }
+        for(let i in layer) {
+            if(layer[i] == max && max > 1)
+                answer.push(i)
+        }
+    })
+    return answer.sort()
+}
