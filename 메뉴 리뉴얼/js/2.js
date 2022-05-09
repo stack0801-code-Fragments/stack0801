902ee4eefcb9b0d75905980c68895049d505dd8b
stack0801 <stack0801@gmail.com>
2022-05-10 00:50:17 +0900

    메뉴 리뉴얼

--- 메뉴 리뉴얼/js/2.js
+++ 메뉴 리뉴얼/js/2.js
@@ -1,33 +1,31 @@
 function solution(orders, course) {
-    const answer = [],
-          list = [];
-    course.map(c => list[c] = {});                  // 코스에 따라 list 분류
-    orders.map(order => {                           // order 에 따라 맞는 코스 경우의 수
-        order = order.split('').sort();             // order 를 배열로
-        let count = Math.pow(2, order.length)       // 이진법을 이용 ex) 00001(2) ~ 11111(2) 
+    const answer = []
+    const list = []
+    orders = orders.map(order => order.split('').sort())
+    course.map(c => list[c] = {})
+    orders.map(order => {
+        let count = Math.pow(2, order.length)
         while(--count > 2) {
-            let str = "",
-                bit = 1;
+            let str = ''
             for(let i = 0; i < order.length; i++) {
-                if(count & bit)
-                    str += order[i];
-                bit <<= 1;
+                if(count & Math.pow(2, order.length - i - 1))
+                    str += order[i]
             }
-            if(list[str.length])                    // 효율을 위해 원하는 코스만 저장
-                list[str.length][str] ? list[str.length][str] += 1 : list[str.length][str] = 1;
+            if(list[str.length])
+                !list[str.length][str] ? list[str.length][str] = 1 : list[str.length][str] += 1
         }
-    })// 코스에서 가장 많이 시킨거 answer 에 추가
-    for(let i in list) {
-        let max = 1
-        for(let j in list[i]) {
-            if(list[i][j] > max)
-                max = list[i][j]
+    })
+    list.map(layer => {
+        let max = 0
+        for(let i in layer) {
+            if(layer[i] > max)
+                max = layer[i]
         }
-        for(let j in list[i]) {
-            if(list[i][j] == max && max > 1)
-                answer.push(j)
+        for(let i in layer) {
+            if(layer[i] == max && max > 1)
+                answer.push(i)
         }
-    }
+    })
     return answer.sort()
 }
 
---
+++ 메뉴 리뉴얼/js/3.js
@@ -0,0 +1,36 @@
+function solution(orders, course) {
+    const answer = [],
+          list = [];
+    course.map(c => list[c] = {});                  // 코스에 따라 list 분류
+    orders.map(order => {                           // order 에 따라 맞는 코스 경우의 수
+        order = order.split('').sort();             // order 를 배열로
+        let count = Math.pow(2, order.length)       // 이진법을 이용 ex) 00001(2) ~ 11111(2) 
+        while(--count > 2) {
+            let str = "",
+                bit = 1;
+            for(let i = 0; i < order.length; i++) {
+                if(count & bit)
+                    str += order[i];
+                bit <<= 1;
+            }
+            if(list[str.length])                    // 효율을 위해 원하는 코스만 저장
+                list[str.length][str] ? list[str.length][str] += 1 : list[str.length][str] = 1;
+        }
+    })
+    for(let i of course) {                          // 코스에서 가장 많이 시킨거 answer 에 추가
+        let max = 1
+        for(let j in list[i]) {
+            if(list[i][j] > max)
+                max = list[i][j];
+        }
+        if(max < 2)
+            continue;
+        for(let j in list[i]) {
+            if(list[i][j] == max)
+                answer.push(j);
+        }
+    }
+    return answer.sort()
+}
+
+console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
